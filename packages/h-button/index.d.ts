51cd305b8505acb83e96a468c7ea4f19d41c5b43
mike.cai <mike.cai@ihr360.com>
2018-06-03 18:51:26 +0800

    first commit

---
+++ packages/h-button/index.d.ts
@@ -0,0 +1,30 @@
+import { ButtonType, ButtonSize } from "antd/lib/button";
+import { BaseButtonProps } from "antd/lib/button/button";
+import * as React from "react";
+
+declare interface IPropsInterface extends BaseButtonProps {
+    href?: string,
+    htmlType?: string,
+    loading?: boolean,
+    disabled?: boolean,
+    target?: string,
+    text?: string,
+    addonLeft?: any,
+    addonRight?: any,
+    onClick?: (event?: React.MouseEvent<any>) => void,
+}
+declare class DefaultProps implements IPropsInterface {
+    type: ButtonType;
+    htmlType: string;
+    size: ButtonSize;
+    disabled: boolean;
+    onClick?: (event?: React.MouseEvent<any>) => void
+}
+
+export default class HButton extends React.Component<IPropsInterface, any> {
+    static defaultProps: any;
+    constructor(props: IPropsInterface);
+    componentDidMount(): void;
+    handleClick: (e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement>) => void;
+    render(): JSX.Element;
+}
